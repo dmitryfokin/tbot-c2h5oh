730d5d15417abeafb03299a22fec00f9b3c470f6
Dmitry Fokin <dkoder.dvl@gmail.com>
2025-04-11 13:21:30 +0300

    Добавил кнопку со списком команд

--- handlers/start.py
+++ handlers/start.py
@@ -2,8 +2,15 @@ from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from keyboards.all_kb import main_kb
+from aiogram.types import BotCommand, BotCommandScopeDefault
 
 start_router = Router()
+
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='start_2', description='Старт 2'),
+                BotCommand(command='start_3', description='Старт 3')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
 
 @start_router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -13,3 +20,4 @@ async def cmd_start(message: Message):
 @start_router.message(F.text == '/start_id')
 async def cmd_start_3(message: Message):
     await message.answer(str(message.from_user.id))
+
